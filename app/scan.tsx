126741c3c88e90da042bb6c03dfe644cba7bb13e
Mohammed Islam <mtislam5757@gmail.com>
2025-05-06 18:53:07 -0400

    sending images to OCR

--- app/scan.tsx
+++ app/scan.tsx
@@ -21,21 +21,34 @@ export default function Scan() {
         router.back(); // Optional: auto-navigate back if denied
         return;
       } 
-
+      
+      
       const res = await ImagePicker.launchCameraAsync({
         quality: 1,
         allowsEditing: false,
         mediaTypes: ['images'],
+        base64: true
       });
 
       if (res.canceled) {
         setCameraOpened(false);
-        router.back(); // Optional: go back if canceled
+        router.back(); 
         return;
       }
 
-      const photoUri = res.assets[0].uri;
-      console.log("✅ Photo taken:", photoUri);
+      const asset = res.assets[0]
+      const dataUrl = `data:image/jpeg;base64,${asset.base64}`;
+
+      console.time("OCR")
+      await fetch('https://divi-backend-krh1.onrender.com', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ image: dataUrl }),
+      });
+      console.timeEnd("OCR")
+      console.log("Sent to OCR!")
+      
+      setCameraOpened(false);
 
       // TODO: Add logic to send photoUri to scanning/preview
       setCameraOpened(false);
@@ -53,7 +66,7 @@ export default function Scan() {
       openCamera();
     }, 500);
     return () => clearTimeout(timer);
-  }, []);
+  });
   
   // When returning to this screen (after cancel, etc.)
   useFocusEffect(
