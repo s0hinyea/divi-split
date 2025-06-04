5962c2b95f44fc28fc68baf47e1f48c76edd0788
Mohammed Islam <mtislam5757@gmail.com>
2025-06-03 23:03:21 -0400

    Update contacts

--- app/contacts.tsx
+++ app/contacts.tsx
@@ -1,7 +1,9 @@
-
+import React, { useState, useEffect} from 'react';
+import { View, Text, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 
 
 export default function chooseContacts(){
-  
+
 }
