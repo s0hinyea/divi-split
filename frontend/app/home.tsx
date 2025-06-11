a9d27c8e0af1f281b49a65c18721611b0ad39b9a
Sadid Ethun <ethun2004@gmail.com>
2025-06-11 00:39:36 -0400

    landing page design

--- frontend/app/home.tsx
+++ frontend/app/home.tsx
@@ -1,30 +1,36 @@
-import { View, StyleSheet, Image, TouchableOpacity } from 'react-native'; 
-import { useRouter } from 'expo-router'; 
-import { Text, Button, Surface } from 'react-native-paper';
-import { styles } from '../styles/homeCss'
+import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, Button, Surface } from "react-native-paper";
+import { styles } from "../styles/homeCss";
 
 export default function Home() {
-  const router = useRouter(); 
+	const router = useRouter();
 
-  return (
-    
-    <View style={styles.container}>
-      <TouchableOpacity 
-      style = {styles.helpButton}
-      onPress={() => {router.push('/help')}}>
-        <Text style={styles.helpButtonText}>?</Text>
-      </TouchableOpacity>
-        <Surface style={styles.surface}>
-          <Image style={styles.logo} source={require('../assets/images/reciept-icon.png')}></Image>
-          <Text variant="headlineLarge" style={styles.title}>D I V I</Text>
-          <Text variant="bodyMedium" style={styles.subtitle}>Split your bills with ease</Text>
-          <Button 
-          mode="contained"
-          onPress={() => {router.push('/expense-splitter')}}
-          style={styles.button}>Login</Button>
-        </Surface>
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			{/* <Surface style={styles.surface}> */}
+			<Image
+				style={styles.bgImage}
+				source={require("../assets/images/floating-divison-signs.png")}
+			></Image>
+			<Text style={styles.title}>Divi</Text>
+			<Text style={styles.subtitle}>Split your bills with ease.</Text>
+			<TouchableOpacity
+				onPress={() => {
+					router.push("/expense-splitter");
+				}}
+				style={styles.button}
+			>
+				<Text style={styles.buttonText}>Get Started for Free</Text>
+			</TouchableOpacity>
+			<View style={styles.loginContainer}>
+				<Text style={styles.smallText}>Already have an account? </Text>
+				<TouchableOpacity>
+					<Text style={styles.loginText}>Log In</Text>
+				</TouchableOpacity>
+			</View>
+
+			{/* </Surface> */}
+		</View>
+	);
 }
-
-
